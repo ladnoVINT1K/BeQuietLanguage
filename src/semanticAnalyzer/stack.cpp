e74bb6c8364d4db2dff7d4307dbce978f0b9aef0
ladnoVINT1K <poiiir985@gmail.com>
2025-11-25 22:44:09 +0300

    bug fix

--- src/semanticAnalyzer/stack.cpp
+++ src/semanticAnalyzer/stack.cpp
@@ -1,16 +1,20 @@
 #pragma once
 #include "stack.h"
+#include <iostream>
 
-type_stack::type_stack() {}
+type_stack::type_stack() {
+    Operators_ = {};
+    Operations_ = {};
+}
 
-void type_stack::push_stack(string lex, int d = 0) {
+void type_stack::push_stack(string lex, int d) {
     if (find(simple_oper.begin(), simple_oper.end(), lex) != simple_oper.end() or
         find(compound_oper.begin(), compound_oper.end(), lex) != compound_oper.end()) {
         Operations_.push_front(lex);
     } else {
         Types t;
         string v = lex;
-        std::regex n("^\d+(\.\d+)?$");
+        std::regex n("^\\d+(\\.\\d+)?$");
         if (std::regex_match(lex, n)) {
             t = Types::Num;
             Operators_.push_front({t, d, v});
@@ -53,17 +57,16 @@ void type_stack::check_uno() {
 
 void type_stack::check_bin() {
     info c1 = Operators_.front(); Operators_.pop_front();
-    info c2 = Operators_.front(); Operators_.pop_front();
+    info c2 = Operators_.front(); Operators_.pop_front();    
     string op = Operations_.front(); Operations_.pop_front();
-
     if (op == "=") {
         if (c1.d_ == c2.d_ and c1.t_ == c2.t_) c2 = c1;
         else throw std::runtime_error("error in assignment");
         push_stack(c2);
         return; 
     }
-
-    if (c1.d_ != 0 or c2.d_ != 0 or c1.t_ != c2.t_) throw std::runtime_error("can't use bin oper for massive");
+    if (c1.t_ != c2.t_) throw std::runtime_error("can't complicit two different types");
+    if (c1.d_ != 0 or c2.d_ != 0) throw std::runtime_error("can't use bin oper for massive");
     else {
         if (c1.t_ == Types::Bool && (op == "==" || op == "!=" || op == "and" || op == "or")) {
             info res(Types::Bool, 0);
@@ -88,8 +91,8 @@ void type_stack::check_bin() {
 }
 
 bool type_stack::check_if() {
-    info c = Operators_.front(); Operations_.pop_front();
-    if (c.t_ == Types::Bool or c.t_ == Types::Bool and c.d_ == 0) return true;
+    info c = Operators_.front(); Operators_.pop_front();
+    if (c.t_ == Types::Bool or c.t_ == Types::Num and c.d_ == 0) return true;
     return false;
 }
 
--- src/semanticAnalyzer/test.cpp
+++ src/semanticAnalyzer/test.cpp
@@ -1,6 +1,16 @@
 #include "tid.cpp"
+#include "stack.cpp"
 #include <iostream>
 
 int main() {
-    
+    type_stack A;
+    A.push_stack("-");
+    A.push_stack("123.123");
+
+    try{ 
+        A.check_uno();
+        std::cout << A.check_if();
+    } catch (std::exception &e) {
+        std::cout << e.what();
+    }
 }
