e24d8291f4f6d227ed074e40c420310cb7654fbb
fsqier <kotovatana456@gmail.com>
2025-12-02 19:51:10 +0300

    Change syntaxer

--- src/syntaxAnalyzer/syntaxer.h
+++ src/syntaxAnalyzer/syntaxer.h
@@ -1,4 +1,7 @@
-#include "../lexicalAnalyzer/lexer.h"
+#include "..\lexicalAnalyzer\lexer.h"
+#include "..\semanticAnalyzer\stack.h"
+#include "..\semanticAnalyzer\tf.h"
+#include "..\semanticAnalyzer\tid.h"
 
 class Syntaxer {
 public:
@@ -10,26 +13,27 @@ public:
 	bool matchType(Types type);
 private:
 	Lexer& lexer_;
+	tree_tid tid_;
+	tf tf_;
+	type_stack stack_;
+	string id_, type_, unic_, expr_;
+	info_func func_;
+	param param_;
+	int depth_;
 	Lexem curr_;
 	void NewToken();
 	void Prog();
 	void Declarations();
-	void Declaration();
 	void Var();
 	void Type();
-	void Init();
 	void Func();
 	void TypeF();
 	void Params();
-	void Param();
 	void FuncR();
 	void State();
 	void MState();
 	void IfState();
-	void IfStateTail();
 	void ForState();
-	void ForStateTail();
-	void A();
 	void WhileState();
 	void ReturnState();
 	void Expr();
@@ -41,12 +45,17 @@ private:
 	void E6();
 	void E7();
 	void E8();
-	void CI();
-	void Call();
-	void CallTail();
-	void Args();
 	void Iden();
 	void list();
 	void cin();
 	void cout();
+	typefunc to_ftype(string type);
+	typefunc to_ftype(TypesId type);
+	typefunc to_ftype(typestack type);
+	typestack to_sttype(string type);
+	TypesId to_idtype(string type);
+	TypesId to_idtype(typefunc type);
+	TypesId to_idtype(typestack type);
+	void check_init(string type1, typestack type2);
+	bool check_return(typefunc type1, typestack type2);
 };
